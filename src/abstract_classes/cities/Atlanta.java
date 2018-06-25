fa810e487aae264f49c5552c03aa9e921d8e3617
League <league@WTS9.attlocal.net>
2018-06-24 17:26:24 -0700

    new module

---
+++ src/abstract_classes/cities/Atlanta.java
@@ -0,0 +1,11 @@
+package abstract_classes.cities;
+
+public class Atlanta extends City {
+
+	@Override
+	double getAnnualTaxes() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/abstract_classes/cities/LosAngeles.java
@@ -0,0 +1,11 @@
+package abstract_classes.cities;
+
+public class LosAngeles extends City {
+
+	@Override
+	double getAnnualTaxes() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
